285bd943efddebedd749e72c7d42844b4cb10bf3
Char-318 <charlotte@mulvey.uk.net>
2021-11-30 18:10:16 +0000

    Reads file and creates node objects

--- SEACW2-PathFinding/Program.cs
+++ SEACW2-PathFinding/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace SEACW2_PathFinding
 {
@@ -6,7 +8,31 @@ namespace SEACW2_PathFinding
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ReadFile("../../../../ACW2_test_data_01.txt");
+        }
+
+        public static List<Node> ReadFile(string fileName)
+        {
+            List<Node> nodes = new List<Node>();
+            StreamReader reader = new StreamReader(fileName);
+
+            reader.ReadLine();
+            string line = reader.ReadLine();
+            
+            while (line != "Edges")
+            {
+                string[] nodeInfo = line.Split(',');
+                int id = int.Parse(nodeInfo[0]);
+                string name = nodeInfo[1].Trim('"');
+                int xCoordinate = int.Parse(nodeInfo[2]);
+                int yCoordinate = int.Parse(nodeInfo[3]);
+                
+                Node node = new Node(id, name, xCoordinate, yCoordinate);
+                nodes.Add(node);
+                line = reader.ReadLine();
+            }
+
+            return nodes;
         }
     }
 }
