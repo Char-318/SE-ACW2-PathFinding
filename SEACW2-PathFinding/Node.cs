c7a3872a092f2837eb089e87820855f41bb96311
Char-318 <charlotte@mulvey.uk.net>
2021-11-30 17:29:53 +0000

    Adding method to add child nodes

--- SEACW2-PathFinding/Node.cs
+++ SEACW2-PathFinding/Node.cs
@@ -1,7 +1,26 @@
+using System.Collections.Generic;
+
 namespace SEACW2_PathFinding
 {
     public class Node
     {
-        
+        private string _name;
+        private int _id;
+        private Dictionary<int, int> _childNodes { get; } = new Dictionary<int, int>();
+        private int _xCoordinate;
+        private int _yCoordinate;
+
+        public Node(int id, string name, int xCoordinate, int yCoordinate)
+        {
+            _id = id;
+            _name = name;
+            _xCoordinate = xCoordinate;
+            _yCoordinate = yCoordinate;
+        }
+
+        public void AddChildNode(int childId, int length)
+        {
+            _childNodes.Add(childId, length);
+        }
     }
 }
