df722ce07b15f88dcec523fb97fb5669bfaab896
Char-318 <charlotte@mulvey.uk.net>
2021-12-01 16:05:23 +0000

    Initialising dijkstra's algorithm class

--- SEACW2-PathFinding/Dijkstra.cs
+++ SEACW2-PathFinding/Dijkstra.cs
@@ -1,7 +1,21 @@
+using System.Collections.Generic;
+
 namespace SEACW2_PathFinding
 {
     public class Dijkstra
     {
-        
+        private Dictionary<Node, int> _unvisited = new Dictionary<Node, int>();
+        private Node _startNode, _endNode;
+
+        public Dijkstra(Node startNode, Node endNode)
+        {
+            foreach (Node node in Program.nodePool.GetNodePool())
+            {
+                _unvisited.Add(node, -1);
+            }
+
+            _startNode = startNode;
+            _endNode = endNode;
+        }
     }
 }
